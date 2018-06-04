01fb0e0f0d7bcc9d4a15995d105007053bebf960
nana <nana.okada7@gmail.com>
2018-06-04 22:44:52 +0900

    画面表示

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <title>TODO LIST</title>
+
+  <!-- CSSとJavaScript -->
+</head>
+
+<body>
+  <div class="container">
+    <nav class="navbar navbar-default">
+      <!-- ナビバーの内容 -->
+      <!-- ヘッダ情報 -->
+      <div class="navbar-header">
+        <a class="navbar-brand">TODO LIST</a>
+      </div>
+
+    </div>
+  </nav>
+</div>
+
+@yield('content')
+</body>
+</html>
