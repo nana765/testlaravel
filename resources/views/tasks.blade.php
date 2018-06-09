1cf8e8a6501db63423c06d054997159e06d37f9c
nana <nana.okada7@gmail.com>
2018-06-09 17:56:20 +0900

    taskビュー

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -2,5 +2,34 @@
 
 @section('content')
 
+<div class="container">
+  <div class="col-sm-offset-0 col-sm-8">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        新規タスク
+      </div>
+      <div class="panel-body">
+        <!-- 新タスクフォーム -->
+          <form>
+              <!-- タスク名 -->
+              <div class="form-group">
+                  <label for="task" class="col-sm-2 control-label">タスク</label>
 
-               @endsection
+                  <div class="col-sm-9">
+                      <input type="text" name="name" id="task-name" class="form-control">
+                  </div>
+              </div>
+
+              <!-- タスク追加ボタン -->
+              <div class="form-group">
+                  <div class="col-sm-offset-10 col-sm-6">
+                      <button type="submit" class="btn btn-default">
+                        タスク追加
+                      </button>
+                  </div>
+              </div>
+          </form>
+
+      </div>
+      </form>
+      @endsection
